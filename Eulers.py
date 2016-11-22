1e2c3a3fa08f1f998c51948dc56410edd701706c
fbriody <fbriody@users.noreply.github.com>
2016-11-22 11:57:40 -0600

    Add files via upload

--- Eulers.py
+++ Eulers.py
@@ -16,6 +16,7 @@ x = np.linspace(0.5, 2.0, 100)
 #y = np.cos(2*np.pi*x) * np.exp(-x)
 y = np.tan(x-1) #* np.exp(-x)
 
+
 plt.plot(x, y, 'b')
 plt.title('Damped exponential decay', fontdict=font)
 plt.text(2, 0.65, r'$\cos(2 \pi t) e^{-t}$', fontdict=font)
@@ -28,8 +29,7 @@ plt.axhline(y=0, color='k')
 plt.axvline(x=0, color='k')
 
 
-#for x in range(0, 3):
-    #print "We're on time %d" % (x)
+#plots function values   
 def my_range(start, end, step):
     while start <= end:
         yield start
@@ -39,23 +39,16 @@ for x in my_range(.5, 2, 0.1):
     y = np.tan(x-1)
     print x,y
     plt.plot(x, y, 'g^')
-    
-#added by FB; generates Euler ordered pairs
-def euler(df, x0, y0, dx, x1):
-    x, y = x0, y0
-    while x < x1:
-        y = y + dx * df(x, y)
-        x += dx
-    return y
 
+#Calculates and plts Euler values
 def fprime(x, y):
     return (y**2) +1
 
-x0 = 1
-y0 = 0
-x1 = 2.0
+x0,y0 = 1,0
+x1 = 1.4
 h=.1
 yi = y0
+xa,ya=[],[]
 #added by FB; generates Euler table values
 def my_range(start, end, step):
     while start <= end:
@@ -63,13 +56,14 @@ def my_range(start, end, step):
         start += step
 
 for xi in my_range(x0,x1,h):
+#   yi=euler(fprime,x0,y0,h,x1)
     print xi,yi,np.tan(xi-1)
+    ya.append(yi)
     plt.plot(xi,yi,'ro')
     yi=yi+fprime(xi,yi)*h
+    xa.append(xi)
 
-
-
-
+print xa,ya
 
 # Tweak spacing to prevent clipping of ylabel
 plt.subplots_adjust(left=0.1)
